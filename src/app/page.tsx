d30518dd594b3c35ec8ac02dc87f33317f8bd9b4
Matthieu Laizé <matthieu.laize.ext@lesechos.fr>
2024-02-27 15:08:24 +0100

    feat: remove all useless files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-'use client';
+"use server";
 
-import React from 'react';
-import { Card } from 'antd';
-import { Col, Row } from 'reactstrap';
-
-import AddTodoForm from '@/components/AddTodoForm';
-import TodoList from '@/components/TodoList';
+import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <Row>
-        <Col xxl="6" xl="6" lg="6" md="12" sm="12" xs="12" className='mt-3'>
-          <Card title="Add Todo">
-            <AddTodoForm />
-          </Card>
-        </Col>
-
-        <Col xxl="6" xl="6" lg="6" md="12" sm="12" xs="12" className='mt-3'>
-          <Card title="Todo List">
-            <TodoList />
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
+  return <div>This is a default page</div>;
 };
 
 export default page;
--- src/theme/themeConfig.ts
+++
@@ -1,9 +0,0 @@
-import type { ThemeConfig } from 'antd';
-
-const theme: ThemeConfig = {
-  token: {
-    colorPrimary: '#ff4d4f',
-  },
-};
-
-export default theme;
